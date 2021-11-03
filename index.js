e4afd809b9b3c5425280fa45bd815a3600a9675f
Noa Udler <udlernoa4@gmail.com>
2021-11-03 10:22:05 -0700

    commit tree eba6a06966aa760a34a16c6be2b8dec3d50e8ae4

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+    let result = [];
+    result.push(arrayOfDrivers[0], arrayOfDrivers[1]);
+    return result;
+}
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+    let result = [];
+    result.push(arrayOfDrivers[arrayOfDrivers.length - 2], arrayOfDrivers[arrayOfDrivers.length - 1]);
+    return result;
+}
+
+const selectingDrivers = [];
+selectingDrivers.unshift(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(integer) {
+   return function (fare) {return integer * fare};
+   }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driversFunction) {  
+  return driversFunction(drivers);
+
+}
+
+
+
